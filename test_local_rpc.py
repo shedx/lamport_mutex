55f7d8c8e9a42bde81f892cf4323fb585b3287c2
shedx <anvardinov@gmail.com>
2017-09-24 22:47:17 +0300

    unit tests added

--- test_local_rpc.py
+++ test_local_rpc.py
@@ -1,71 +1,73 @@
+import random
 import unittest
 import os
 import subprocess
 import time
 import sys
 
-from spawn_local import LocalTransport
-from rpc.constants import INTRODUCTION
+from lamport.lamport import Lamport
 
+from rpc.constants import STDIN, REMOTE
+
+
+class MockedRPC:
+    def __init__(self):
+        self.id = 1
+        self.addrs = [('0.0.0.0', 12922 + i) for i in range(5)]
+        self.fd_by_host = {key: i for key, i in zip(self.addrs, range(5))}
+
+    def send_to_fd(self, *args, **kwargs):
+        pass
+
+    def broadcast(self, *args, **kwargs):
+        pass
+
+    def run(self):
+
+        return [(STDIN, {'callback': 'ACQUIRE'})]
 
 class LocalTest(unittest.TestCase):
     def setUp(self):
-        self.pipes = []
-        for _ in range(2):
-            self.pipes.append(os.pipe())
-
-        self.process = subprocess.Popen(
-            [
-                'python3',
-                'spawn_local.py',
-                '-i', str(1)
-            ],
-            stdin=self.pipes[0][0],
-            stderr=sys.stderr,
-            stdout=self.pipes[1][1],
-        )
-
-        self.write = self.pipes[0][1]
-        self.read = self.pipes[1][0]
-        time.sleep(6)
-        LocalTransport.send(
-            self.write,
-            {
-                'callback': INTRODUCTION,
-                'id': 0
-            }
-        )
-
-    def tearDown(self):
-        self.process.kill()
+        self.rpc = MockedRPC()
+        self.lamport = Lamport(self.rpc, 'mutex.txt')
 
     def test_acquire(self):
-        LocalTransport.send(
-            self.write,
-            {
-                'callback': 'ACQUIRE',
-                'id': 0
-            }
+        self.lamport.run()
+        self.assertEqual(
+            self.lamport.unreceived_ack,
+            set([addr for addr in self.rpc.addrs if addr != self.rpc.addrs[1]])
         )
-
-        data = LocalTransport.read(self.read)
-        self.assertEqual(data, {'callback': 'REQUEST', 'clock': 0, 'id': 1})
-        data = LocalTransport.read(self.read)
-        self.assertEqual(data, {'callback': 'RELEASE', 'clock': 1, 'id': 1})
+        self.assertTrue(len(self.lamport.queue) >= 1)
 
     def test_stressmode(self):
-        LocalTransport.send(
-            self.write,
-            {
-                'callback': 'STRESSMODE_START',
-                'id': 0
-            }
-        )
+        def run():
+            return [(STDIN, {'callback': 'STRESSMODE_START'})]
+        self.rpc.run = run
+        self.lamport.run()
+        self.assertEqual(self.lamport.stress_mode, True)
 
-        data = LocalTransport.read(self.read)
-        self.assertEqual(data, {'callback': 'REQUEST', 'clock': 2, 'id': 1})
-        data = LocalTransport.read(self.read)
-        self.assertEqual(data, {'callback': 'RELEASE', 'clock': 3, 'id': 1})
+    def test_request_handler(self):
+        def run():
+            return [(REMOTE, {'callback': 'REQUEST', 'clocks': 10000, 'host': self.rpc.addrs[2]})]
+        self.rpc.run = run
+        queue_len = len(self.lamport.queue)
+        self.lamport.run()
+        self.assertEqual(self.lamport.clock, 10001)
+        self.assertTrue(len(self.lamport.queue) > queue_len)
+
+    def test_check(self):
+        def run():
+            return [(REMOTE, {'callback': 'RELEASE', 'clocks': 100, 'host': self.rpc.addrs[2]})]
+        self.rpc.run = run
+        self.lamport.unreceived_ack.clear()
+        self.lamport.queue = [(random.randint(1, 100), self.rpc.id)]
+        self.lamport.run()
+        self.assertEqual(self.lamport.queue, [])
+
+        with open('mutex.txt', 'r') as file:
+            lines = file.readlines()
+        self.assertEqual(lines[-1].split()[1], str(self.rpc.id))
+        self.assertEqual(lines[-1].split()[-1], 'unlocked')
 
 
 if __name__ == '__main__':
